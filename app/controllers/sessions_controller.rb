84462eb72f097bc54636f4d5ce1ae7b2317e2a2a
Hope Gochnour <hope.gochnour@gmail.com>
2021-04-17 14:16:06 -0600

    oauth complete
    Co-authored-by: Jeremiah Michlitsch 
    <61896482+jmichlitsch@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,26 @@ class SessionsController < ApplicationController
     conn = Faraday.new(url: 'https://api.kroger.com/v1/', )
     conn.basic_auth(client_id, client_secret)
 
-    response = conn.post('connect/oauth2/token', "grant_type=client_credentials&code=#{code}&redirect_uri=#{redirect_uri}")
+    response = conn.post('connect/oauth2/token', "grant_type=authorization_code&code=#{code}&redirect_uri=#{redirect_uri}")
 
     data = JSON.parse(response.body, symbolize_names: true)
     access_token = data[:access_token]
 
-    binding.pry
+    conn = Faraday.new(
+    url: 'https://api.kroger.com/v1/',
+    headers: {
+        'Authorization': "Bearer #{access_token}"
+    })
 
+    response = conn.get('identity/profile')
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    user          = User.find_or_create_by(uid: data[:data][:id])
+    user.uid      = data[:data][:id]
+    user.token    = access_token
+    user.save
+
+    session[:user_id] = user.id
+    redirect_to dashboard_path
   end
 end
