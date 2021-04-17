5be5c49fd888308326ad7909d47d864c2fe88cab
Hope Gochnour <hope.gochnour@gmail.com>
2021-04-17 13:22:48 -0600

    add working oauth config
    Co-authored-by: Jeremiah Michlitsch 
    <61896482+jmichlitsch@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,11 @@ class SessionsController < ApplicationController
     client_secret = ENV['kroger_client_secret']
     code = params[:code]
     redirect_uri = ENV['kroger_redirect_uri']
-    authorization_hash = ("#{client_id}:#{client_secret}")
 
-    conn = Faraday.new(url: 'https://api.kroger.com/v1/')
+    conn = Faraday.new(url: 'https://api.kroger.com/v1/', )
+    conn.basic_auth(client_id, client_secret)
 
-    response = conn.post('connect/oauth2/token') do |req|
-      req.headers['authorization'] = Base64.strict_encode64(authorization_hash),
-      # req.body['content-type'] = 'application/x-www-form-urlencoded',
-      req.body = "grant_type=client_credentials&code=#{code}&redirect_uri=http://localhost:3000/auth/kroger/callback"
-    end
+    response = conn.post('connect/oauth2/token', "grant_type=client_credentials&code=#{code}&redirect_uri=#{redirect_uri}")
 
     data = JSON.parse(response.body, symbolize_names: true)
     access_token = data[:access_token]
