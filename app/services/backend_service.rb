36ae5af64d8fa7e4f2db338285f88b11f07d58c0
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-19 11:06:15 -0600

    Add versioning to endpoint

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,12 +1,12 @@
 class BackendService
   def self.recipe_data(id)
     response = conn.get("/recipes/#{id}")
-
+    require "pry"; binding.pry
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
   def self.conn
-    Faraday.new(url: "https://nameless-plains-48795.herokuapp.com")
+    Faraday.new(url: "https://nameless-plains-48795.herokuapp.com/api/v1")
   end
 end
