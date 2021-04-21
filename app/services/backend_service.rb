fb317484370ddfd76353f393af73ee9ae1fdbbdf
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-04-20 19:35:15 -0600

    Co-authored-by: Harrison Blake <72946334+harrison-blake@users.noreply.github.com>

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,12 +1,23 @@
 class BackendService
   def self.recipe_data(id)
+    #response = conn.get("/recipes/#{id}")
     response = conn.get("/recipes/#{id}")
+    json_parse(response)
+  end
 
-    JSON.parse(response.body, symbolize_names: true)
+  def self.recipe_search_data(ingredient)
+    #conn.get("/recipes?#{ingredient}")
+    response = conn.get("/recipes/complexSearch?apiKey=296c69ea4ec3407d848370782126a86d&includeIngredients=#{ingredient}")
+    json_parse(response)
   end
 
   private
   def self.conn
-    Faraday.new(url: "https://nameless-plains-48795.herokuapp.com")
+    #https://nameless-plains-48795.herokuapp.com/api/v1/
+    Faraday.new(url: "https://api.spoonacular.com")
+  end
+
+  def self.json_parse(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
